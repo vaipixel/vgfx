1d4536cbcac7deecba29e54b8db2319428c930cf
Anatta <1@fxcdev.com>
2024-01-12 23:13:25 +0800

    <feat> Add ColorType.h

---
+++ include/vgfx/core/ColorType.h
@@ -0,0 +1,57 @@
+//
+// Created by Anatta Feng on 2024/1/12.
+//
+#pragma once
+
+namespace vgfx {
+    /**
+     * Describes how pixel bits encode color.
+     */
+    enum class ColorType {
+        /**
+         * uninitialized.
+         */
+        Unknown,
+        /**
+         * Each pixel is stored as a single translucency (alpha) channel. This is very useful for storing
+         * masks efficiently, for instance. No color information is stored. With this configuration, each
+         * pixel requires 1 byte of memory.
+         */
+        ALPHA_8,
+        /**
+         * Each pixel is stored on 4 bytes. Each channel (RGB and alpha for translucency) is stored with 8
+         * bits of precision (256 possible values). The channel order is: red, green, blue, alpha.
+         */
+        RGBA_8888,
+        /**
+         * Each pixel is stored on 4 bytes. Each channel (RGB and alpha for translucency) is stored with 8
+         * bits of precision (256 possible values). The channel order is: blue, green, red, alpha.
+         */
+        BGRA_8888,
+        /**
+         * Each pixel is stored on 2 bytes, and only the RGB channels are encoded: red is stored with 5
+         * bits of precision (32 possible values), green is stored with 6 bits of precision (64 possible
+         * values), and blue is stored with 5 bits of precision.
+         */
+        RGB_565,
+        /**
+         * Each pixel is stored as a single grayscale level. No color information is stored. With this
+         * configuration, each pixel requires 1 byte of memory.
+         */
+        Gray_8,
+        /**
+         * Each pixel is stored on 8 bytes. Each channel (RGB and alpha for translucency) is stored as a
+         * half-precision floating point value. This configuration is particularly suited for wide-gamut
+         * and HDR content.
+         */
+        RGBA_F16,
+        /**
+         * Each pixel is stored on 4 bytes. Each RGB channel is stored with 10 bits of precision (1024
+         * possible values). There is an additional alpha channel that is stored with 2 bits of precision
+         * (4 possible values). This configuration is suited for wide-gamut and HDR content which does not
+         * require alpha blending, such that the memory cost is the same as RGBA_8888 while enabling
+         * higher color precision.
+         */
+        RGBA_1010102
+    };
+} // namespace vgfx
--- include/vgfx/gpu/Surface.h
+++ include/vgfx/gpu/Surface.h
@@ -6,15 +6,41 @@
 #include "vgfx/core/Canvas.h"
 #include "Context.h"
 #include "SurfaceOptions.h"
+#include "vgfx/core/ColorType.h"
 
 namespace vgfx {
 
-
+    /**
+     * The Surface class is responsible for managing the pixels that a Canvas draws into. The Surface
+     * takes care of allocating a Canvas that will draw into the surface. Call surface->getCanvas() to
+     * use that Canvas (but don't delete it, it is owned by the surface). The Surface always has
+     * non-zero dimensions. If there is a request for a new surface, and either of the requested
+     * dimensions is zero, then nullptr will be returned.
+     */
     class Surface {
     public:
-        static std::shared_ptr<Surface>
-        Make(Context *context, int width, int height, bool alphaOnly = false, int sampleCount = 1,
-             bool mipMapped = false, const SurfaceOptions *options = nullptr);
+        /**
+         * Create a new Surface on GPU indicated by context. Allocates memory for pixels based on the
+         * width, height, and color type (alpha only). A Surface with MSAA enabled is returned if the
+         * sample count is greater than 1. Return nullptr if the size is invalid or the alpha-only
+         * textures are not renderable in the GPU backend.
+         * @param context
+         * @param width
+         * @param height
+         * @param alphaOnly
+         * @param sampleCount
+         * @param mipMapped
+         * @param options
+         * @return
+         */
+        static std::shared_ptr<Surface> Make(Context *context, int width, int height,
+                                             bool alphaOnly = false, int sampleCount = 1,
+                                             bool mipMapped = false,
+                                             const SurfaceOptions *options = nullptr);
+
+        static std::shared_ptr<Surface> Make(Context *context, int width, int height, ColorType colorType,
+                                             int sampleCount = 1, bool mipMapped = false,
+                                             const SurfaceOptions *options = nullptr);
 
     };
 
